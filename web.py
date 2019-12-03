6cfb743aa2c40660d7825e03d7fedcd39d26593b
santiagop06 <58007519+santiagop06@users.noreply.github.com>
2019-12-03 11:36:02 -0600

    Add files via upload

--- web.py
+++ web.py
@@ -4,8 +4,21 @@ from selenium import webdriver
 #cap = DesiredCapabilities().Firefox
 #cap["marionette"] = False
 
-driver = webdriver.Firefox()
-driver.get("http://172.17.118.235/")
-button = driver.find_element_by_xpath('/html[1]/body[1]/input[1]')
+
+
+from selenium.webdriver.chrome.options import Options
+
+
+executable_path = "/Users/ASUS-X405U/chromedriver.exe"
+
+
+chrome_options = Options()
+chrome_options.add_extension('/Users/ASUS-X405U/Documents/Metamask.crx')
+
+driver = webdriver.Chrome(executable_path=executable_path, chrome_options=chrome_options)
+
+
+driver.get("http://192.168.0.24:3000/automoviles.html")
+button = driver.find_element_by_xpath('/html[1]/body[1]/section[1]/div[2]/div[3]/button[1]')
 button.click()
 
